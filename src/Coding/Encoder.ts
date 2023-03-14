16866ce3f9e4ce913419684a48699f6197b149fa
ObuchiYuki <yukibochi1@gmail.com>
2023-03-14 11:04:59 +0900

    add encoder

--- src/Coding/Encoder.ts
+++ src/Coding/Encoder.ts
@@ -349,3 +349,261 @@ export class Encoder {
     }
 }
 
+
+
+/**
+ * Now come a few stateful encoder that have their own classes.
+ */
+
+/**
+ * Basic Run Length Encoder - a basic compression implementation.
+ *
+ * Encodes [1,1,1,7] to [1,3,7,1] (3 times 1, 1 time 7). This encoder might do more harm than good if there are a lot of values that are not repeated.
+ *
+ * It was originally used for image compression. Cool .. article http://csbruce.com/cbm/transactor/pdfs/trans_v7_i06.pdf
+ */
+export class RleEncoder<T extends number> extends Encoder {
+    writer: (encoder: Encoder, value: T) => void
+    state: T | null = null
+    count = 0
+
+    constructor(writer: (encoder: Encoder, value: T) => void) {
+        super()
+        this.writer = writer
+    }
+  
+    write(value: T) {
+        if (this.state === value) {
+            this.count++
+        } else {
+            if (this.count > 0) {
+                // flush counter, unless this is the first value (count = 0)
+                this.writeVarUint(this.count - 1) // since count is always > 0, we can decrement by one. non-standard encoding ftw
+            }
+            this.count = 1
+            // write first value
+            this.writer(this, value)
+            this.state = value
+        }
+    }
+}
+  
+/**
+ * Basic diff decoder using variable length encoding.
+ *
+ * Encodes the values [3, 1100, 1101, 1050, 0] to [3, 1097, 1, -51, -1050] using writeVarInt.
+ */
+export class IntDiffEncoder extends Encoder {
+    state: number
+
+    constructor(start: number) {
+        super()
+        this.state = start
+    }
+
+    write(value: number) {
+        this.writeVarInt(value - this.state)
+        this.state = value
+    }
+}
+  
+/**
+ * A combination of IntDiffEncoder and RleEncoder.
+ *
+ * Basically first writes the IntDiffEncoder and then counts duplicate diffs using RleEncoding.
+ *
+ * Encodes the values [1,1,1,2,3,4,5,6] as [1,1,0,2,1,5] (RLE([1,0,0,1,1,1,1,1]) ⇒ RleIntDiff[1,1,0,2,1,5])
+ */
+export class RleIntDiffEncoder extends Encoder {
+    state: number
+    count = 0
+
+    constructor(start: number) {
+        super()
+        this.state = start
+    }
+
+    write(value: number) {
+        if (this.state === value && this.count > 0) {
+        this.count++
+        } else {
+            if (this.count > 0) {
+                // flush counter, unless this is the first value (count = 0)
+                this.writeVarUint(this.count - 1) // since count is always > 0, we can decrement by one. non-standard encoding ftw
+            }
+            this.count = 1
+            // write first value
+            this.writeVarInt(value - this.state)
+            this.state = value
+        }
+    }
+}
+ 
+/**
+ * Optimized Rle encoder that does not suffer from the mentioned problem of the basic Rle encoder.
+ *
+ * Internally uses VarInt encoder to write unsigned integers. If the input occurs multiple times, we write
+ * write it as a negative number. The UintOptRleDecoder then understands that it needs to read a count.
+ *
+ * Encodes [1,2,3,3,3] as [1,2,-3,3] (once 1, once 2, three times 3)
+ */
+export class UintOptRleEncoder {
+    encoder = new Encoder()
+    state = 0
+    count = 0
+    
+    constructor () {}
+
+    write(value: number) {
+        if (this.state === value) {
+            this.count++    
+        } else {
+            UintOptRleEncoder.flush(this)
+            this.count = 1
+            this.state = value
+        }
+    }
+
+    toUint8Array () {
+        UintOptRleEncoder.flush(this)
+        return this.encoder.toUint8Array()
+    }
+
+    static flush(encoder: UintOptRleEncoder) {
+        if (encoder.count > 0) {
+            // flush counter, unless this is the first value (count = 0)
+            // case 1: just a single value. set sign to positive
+            // case 2: write several values. set sign to negative to indicate that there is a length coming
+            encoder.encoder.writeVarInt(encoder.count === 1 ? encoder.state : -encoder.state)
+    
+            if (encoder.count > 1) {
+                encoder.encoder.writeVarUint(encoder.count - 2) // since count is always > 1, we can decrement by one. non-standard encoding ftw
+            }
+        }
+    }    
+}
+  
+/**
+ * Increasing Uint Optimized RLE Encoder
+ *
+ * The RLE encoder counts the number of same occurences of the same value.
+ * The IncUintOptRle encoder counts if the value increases.
+ * I.e. 7, 8, 9, 10 will be encoded as [-7, 4]. 1, 3, 5 will be encoded
+ * as [1, 3, 5].
+ */
+export class IncUintOptRleEncoder {
+    encoder = new Encoder()
+    state = 0
+    count = 0
+    constructor () {}
+
+    write(value: number) {
+        if (this.state + this.count === value) {
+            this.count++
+        } else {
+            UintOptRleEncoder.flush(this)
+            this.count = 1
+            this.state = value
+        }
+    }
+
+    toUint8Array () {
+        UintOptRleEncoder.flush(this)
+        return this.encoder.toUint8Array()
+    }
+}
+  
+/**
+ * A combination of the IntDiffEncoder and the UintOptRleEncoder.
+ *
+ * The count approach is similar to the UintDiffOptRleEncoder, but instead of using the negative bitflag, it encodes
+ * in the LSB whether a count is to be read. Therefore this Encoder only supports 31 bit integers!
+ *
+ * Encodes [1, 2, 3, 2] as [3, 1, 6, -1] (more specifically [(1 << 1) | 1, (3 << 0) | 0, -1])
+ *
+ * Internally uses variable length encoding. Contrary to normal UintVar encoding, the first byte contains:
+ * * 1 bit that denotes whether the next value is a count (LSB)
+ * * 1 bit that denotes whether this value is negative (MSB - 1)
+ * * 1 bit that denotes whether to continue reading the variable length integer (MSB)
+ *
+ * Therefore, only five bits remain to encode diff ranges.
+ *
+ * Use this Encoder only when appropriate. In most cases, this is probably a bad idea.
+ */
+export class IntDiffOptRleEncoder {
+    encoder = new Encoder()
+    state = 0
+    count = 0
+    diff = 0
+
+    constructor() {}
+
+    write(value: number) {
+        if (this.diff === value - this.state) {
+            this.state = value
+            this.count++
+        } else {
+            this.flush()
+            this.count = 1
+            this.diff = value - this.state
+            this.state = value
+        }
+    }
+
+    toUint8Array () {
+        this.flush()
+        return this.encoder.toUint8Array()
+    }
+
+        
+    flush() {
+        if (this.count > 0) {
+            // 31 bit making up the diff | wether to write the counter
+            // const encodedDiff = encoder.diff << 1 | (encoder.count === 1 ? 0 : 1)
+            const encodedDiff = this.diff * 2 + (this.count === 1 ? 0 : 1)
+            // flush counter, unless this is the first value (count = 0)
+            // case 1: just a single value. set first bit to positive
+            // case 2: write several values. set first bit to negative to indicate that there is a length coming
+            this.encoder.writeVarInt(encodedDiff)
+            if (this.count > 1) {
+                this.encoder.writeVarUint(this.count - 2) // since count is always > 1, we can decrement by one. non-standard encoding ftw
+            }
+        }
+    }
+}
+  
+/**
+ * Optimized String Encoder.
+ *
+ * Encoding many small strings in a simple Encoder is not very efficient. The function call to decode a string takes some time and creates references that must be eventually deleted.
+ * In practice, when decoding several million small strings, the GC will kick in more and more often to collect orphaned string objects (or maybe there is another reason?).
+ *
+ * This string encoder solves the above problem. All strings are concatenated and written as a single string using a single encoding call.
+ *
+ * The lengths are encoded using a UintOptRleEncoder.
+ */
+export class StringEncoder {
+    sarr: string[] = []
+    state = ''
+    lensE = new UintOptRleEncoder()
+    
+    constructor () {}
+
+    write(string: string) {
+        this.state += string
+        if (this.state.length > 19) {
+            this.sarr.push(this.state)
+            this.state = ''
+        }
+        this.lensE.write(string.length)
+    }
+
+    toUint8Array() {
+        const encoder = new Encoder()
+        this.sarr.push(this.state)
+        this.state = ''
+        encoder.writeVarString(this.sarr.join(''))
+        encoder.writeUint8Array(this.lensE.toUint8Array())
+        return encoder.toUint8Array()
+    }
+}
