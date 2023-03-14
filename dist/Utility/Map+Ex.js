f46ad54503038a0f05073a28ec1958d8e7c518e6
ObuchiYuki <yukibochi1@gmail.com>
2023-03-14 17:46:52 +0900

    --

--- dist/Utility/Map+Ex.js
+++ dist/Utility/Map+Ex.js
@@ -1,4 +1,7 @@
 "use strict";
+if (Map.prototype.setIfUndefined != null) {
+    console.error("Map.prototype.setIfUndefined is already non null.");
+}
 Map.prototype.setIfUndefined = function (key, make) {
     const value = this.get(key);
     if (value != null)
--- src/Utility/Map+Ex.ts
+++ src/Utility/Map+Ex.ts
@@ -3,6 +3,9 @@ interface Map<K, V> {
     setIfUndefined(key: K, make: () => V): V;
 }
 
+if (Map.prototype.setIfUndefined != null) {
+    console.error("Map.prototype.setIfUndefined is already non null.")
+}
 Map.prototype.setIfUndefined = function<K, V>(key: K, make: () => V): V {
     const value = this.get(key)
     if (value != null) return value
